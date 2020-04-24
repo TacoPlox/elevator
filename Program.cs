d2b455a357a3ecf2127eacc00a6598bcfdaeac68
Hiram Hernandez <ehiram.hr@gmail.com>
2020-04-24 13:54:09 -0600

    Elevator implemented.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace elevator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu menu = new Menu();
+            menu.Display();
+        }
+    }
+}
